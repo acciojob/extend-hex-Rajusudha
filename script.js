36ec12926994ca311a798da3ec8cbad78bdc25ec
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-08 09:05:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
+function extendHex(shortHex) {
+  // Remove the '#' symbol if present and convert to lowercase
+  shortHex = shortHex.replace(/^#/, '').toLowerCase();
 
-// Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+  // Ensure the input is a valid short hex code
+  if (!/^[0-9a-f]{3}$/i.test(shortHex)) {
+    console.error('Invalid short hex code');
+    return null;
+  }
+
+  // Extend the short hex code to the full hex code
+  const fullHex = shortHex
+    .split('')
+    .map(component => component.repeat(2))
+    .join('');
+
+  return `#${fullHex}`;
+}
+
+// Examples
+console.log(extendHex("#abc"));   // expected output: "#aabbcc"
+console.log(extendHex("abc"));    // expected output: "#aabbcc"
+console.log(extendHex("#AbC"));   // expected output: "#AABBCC"
+console.log(extendHex("#f09"));   // expected output: "#ff0099"
+
